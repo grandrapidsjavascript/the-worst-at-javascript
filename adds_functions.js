bf2ff2b1234443428e07aaf19715a4b334dcad72
kendell joseph <kendell@jct.io>
2018-02-08 01:30:39 +0000

    Adds initial js files

---
+++ adds_functions.js
@@ -0,0 +1,6 @@
+// Add a function to this that adds two numbers
+module.exports = {
+  addTwoNumbers: function(number1, number2){
+    return number1 + number2;
+  }
+};
---
+++ run_adds.js
@@ -0,0 +1,4 @@
+console.log("Running adds functions...");
+const functions = require("./adds_functions");
+const result1 = functions.addTwoNumbers(1, 1);
+console.log(result1);
